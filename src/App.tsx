9ffd9fcd30b81e763f36d15312beaf8f51549753
= <=>
2023-05-19 01:53:26 +0300

    add map and yandex ui kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { MyMap } from './сomponents/map';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 
 function App() {
   return (
     <div className="App">
-      hello
+      <MyMap></MyMap>
     </div>
   );
 }
