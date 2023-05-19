881e16a70ef7211738631a7632b723027750b620
ilia <il.vsl0110@gmail.com>
2023-05-19 22:52:31 +0300

    remove bugs

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { configureRootTheme } from '@yandex/ui/Theme'
 import { theme } from '@yandex/ui/Theme/presets/default'
 
 
-configureRootTheme({ theme })
 import { MyMap } from './сomponents/map';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+configureRootTheme({ theme })
 
 function App() {
   return (
